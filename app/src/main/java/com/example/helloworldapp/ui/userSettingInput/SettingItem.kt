2aada080ac6f5a235f01f0a73cb4e02bcd83a65f
Kohchaaa <koustar2004@gmail.com>
2025-12-08 01:21:56 +0900

    feat: settingItemを汎用コンポーネントにした

---
+++ app/src/main/java/com/example/helloworldapp/ui/userSettingInput/SettingItem.kt
@@ -0,0 +1,31 @@
+package com.example.helloworldapp.ui.userSettingInput
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun SettingItem(itemName: String, content: @Composable () -> Unit) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = Color.DarkGray)
+            .padding(horizontal = 20.dp, vertical = 20.dp)
+    ) {
+        Text(
+            text = itemName,
+            modifier = Modifier.padding(end = 16.dp)
+        )
+        content()
+    }
+}
