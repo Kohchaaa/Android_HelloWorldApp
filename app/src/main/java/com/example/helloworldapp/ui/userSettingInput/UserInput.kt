0281afbded8dbf47e92a29d28a965f9694a4b7ba
Kohchaaa <koustar2004@gmail.com>
2025-12-11 16:57:20 +0900

    feat: UserInputをSerializableにした

--- app/src/main/java/com/example/helloworldapp/ui/userSettingInput/UserInput.kt
+++ app/src/main/java/com/example/helloworldapp/ui/userSettingInput/UserInput.kt
@@ -1,11 +1,23 @@
 package com.example.helloworldapp.ui.userSettingInput
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 import java.util.UUID
 
+@Serializable
 data class UserInput(
     val userId: String = UUID.randomUUID().toString(),
     val displayName: String,
+    @Serializable(with = LocalDateSerializer::class)
     val birthDate: LocalDate?,
     val gender: Gender?,
     val allergies: Set<String>,
@@ -13,10 +25,27 @@ data class UserInput(
     val dislikeDishes: List<String>,
     val customAttributes: Map<String, String>?, // Key-value形式で柔軟に選択肢して保存（カスタムアトリビュート型作った方が良いのかな）
     val intakeTargetVersion: String
-) {
+)
 
+// 1. LocalDate用のカスタムシリアライザーを作成
+object LocalDateSerializer : KSerializer<LocalDate> {
+    // 日付のフォーマットを定義 (YYYY-MM-DD)
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val formatter = DateTimeFormatter.ISO_LOCAL_DATE
 
+    // このシリアライザーがどのようなデータ型（プリミティブ型）に変換されるかを定義
+    override val descriptor: SerialDescriptor =
+        PrimitiveSerialDescriptor("LocalDate", PrimitiveKind.STRING)
 
+    // シリアライズ処理：LocalDateオブジェクトをStringに変換する
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun serialize(encoder: Encoder, value: LocalDate) {
+        encoder.encodeString(value.format(formatter))
+    }
+
+    // デシリアライズ処理：StringをLocalDateオブジェクトに変換する
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun deserialize(decoder: Decoder): LocalDate {
+        return LocalDate.parse(decoder.decodeString(), formatter)
+    }
 }
-
-
