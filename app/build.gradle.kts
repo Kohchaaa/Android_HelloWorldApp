32f9db156cad5ee151ba636860e02adce1b122c9
Kohchaaa <koustar2004@gmail.com>
2025-12-14 15:34:38 +0900

    package: lifecycle-viewmodel-composeの追加
    - Project Structureで

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.androidx.datastore)
     implementation(libs.androidx.navigation.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
