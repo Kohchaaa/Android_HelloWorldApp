062001b9058b45d8c1f0a9c2f191a58b541a76c9
Kohchaaa <koustar2004@gmail.com>
2025-12-23 15:05:08 +0900

    chore: annotation付けた

--- app/src/main/java/com/example/helloworldapp/navigation/NavigationScreen.kt
+++ app/src/main/java/com/example/helloworldapp/navigation/NavigationScreen.kt
@@ -1,5 +1,7 @@
 package com.example.helloworldapp.navigation
 
+import android.os.Build
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -7,6 +9,7 @@ import androidx.navigation.compose.rememberNavController
 import com.example.helloworldapp.feature.main.MainScreen
 import com.example.helloworldapp.feature.setting.SettingScreen
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun NavigationScreen() {
     val navController = rememberNavController()
