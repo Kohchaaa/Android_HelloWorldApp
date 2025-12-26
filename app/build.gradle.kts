5a8510efcef5a7724d3f09841b3f3cbac3986512
Kohchaaa <koustar2004@gmail.com>
2025-12-26 17:46:11 +0900

    package: kizitonwose/Calendarライブラリを追加

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,7 @@ dependencies {
     implementation(libs.androidx.datastore)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.calendar.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
