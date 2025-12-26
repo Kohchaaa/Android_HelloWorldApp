a60ffce37ae6b7da1a72848f842c6f7d7d697b35
Kohchaaa <koustar2004@gmail.com>
2025-12-26 15:22:47 +0900

    ui: Androidのナビゲーションバーに，アプリのNavigationBarが被らないようにした

--- app/src/main/java/com/example/helloworldapp/feature/main/NavigationBar.kt
+++ app/src/main/java/com/example/helloworldapp/feature/main/NavigationBar.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
@@ -26,6 +29,7 @@ fun NavigationBar(
         modifier = Modifier
             .fillMaxWidth()
             .background(MaterialTheme.colorScheme.surface)
+            .windowInsetsPadding(WindowInsets.navigationBars)
     ) {
         MainScreenTabs.entries.forEachIndexed { index, tab ->
 
