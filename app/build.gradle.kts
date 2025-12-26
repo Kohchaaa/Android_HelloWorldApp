88771bff990a1270b60f323ce0d3b5de3a8f395b
Kohchaaa <koustar2004@gmail.com>
2025-12-27 00:55:59 +0900

    package: materialIconsExtendedを追加
    - お手軽に沢山アイコンを使いたかったんや

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,6 +60,7 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.calendar.compose)
+    implementation(libs.androidx.compose.material.icons.extended)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
