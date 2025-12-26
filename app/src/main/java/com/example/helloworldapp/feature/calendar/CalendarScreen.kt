fe05d9a038a47de20b0a38c59538e469fd266ddd
Kohchaaa <koustar2004@gmail.com>
2025-12-27 00:31:30 +0900

    refactor: rememberを使って食事レコードの計算をキャッシュさせて軽量化
    - rememberを使うことで，rememberの中にあるデータ（今回はmealRecordなど）が変わらない限りキャッシュを使うように
    - 前はmap変換とかが，スクロールのフレーム単位レベルで実行されてた
    - キャッシュを使うことで良い感じに描画
    - 今回はモックを使ってるので最初に取得したら一切その後はデータが変わらない

--- app/src/main/java/com/example/helloworldapp/feature/calendar/CalendarScreen.kt
+++ app/src/main/java/com/example/helloworldapp/feature/calendar/CalendarScreen.kt
@@ -1,11 +1,16 @@
 import android.annotation.SuppressLint
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -13,11 +18,13 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.helloworldapp.data.mock.mockMealRecord
 import com.example.helloworldapp.feature.calendar.DailyMealStatus
 import com.example.helloworldapp.feature.calendar.MealCalendar
+import com.example.helloworldapp.type.MealRecord
 import com.example.helloworldapp.type.MealType
 import com.example.helloworldapp.ui.theme.HelloWorldAppTheme
 import com.kizitonwose.calendar.compose.rememberCalendarState
@@ -54,11 +61,14 @@ fun CalendarScreen(
 
         val mealRecord = mockMealRecord
 
+        val dailyRecord: Map<LocalDate, List<MealRecord>> = remember(mealRecord) {
+            mealRecord.groupBy { it.date.toLocalDate() }
+        }
+
         // 食事レコードを，カレンダー表示に最適なデータに
         // 日付をキーとして，その日のMealTypeごとに食べたかどうかが真偽値で保存
-        val mealStatus: Map<LocalDate, DailyMealStatus> = mealRecord
-            .groupBy { it.date.toLocalDate() }
-            .mapValues { (_, records) ->
+        val mealStatus: Map<LocalDate, DailyMealStatus> = remember(dailyRecord) {
+            dailyRecord.mapValues { (_, records) ->
                 DailyMealStatus(
                     hasBreakfast = records.any { it.mealType == MealType.BREAKFAST },
                     hasLunch = records.any { it.mealType == MealType.LUNCH },
@@ -67,9 +77,9 @@ fun CalendarScreen(
                     hasMidnight = records.any { it.mealType == MealType.MIDNIGHT }
                 )
             }
+        }
 
         Column(
-            //verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.fillMaxSize()
         ) {
@@ -82,6 +92,25 @@ fun CalendarScreen(
             )
 
             HorizontalDivider(thickness = 2.dp)
+            Spacer(Modifier.height(8.dp))
+
+
+            val selectedMealRecord = dailyRecord[selectedDate] ?: emptyList()
+
+            if (selectedMealRecord.isNotEmpty()) {
+                LazyColumn(
+
+                ) {
+                    items(selectedMealRecord.size) { index ->
+                        val record = selectedMealRecord[index]
+                        Text(record.mealName)
+                    }
+                }
+            } else {
+                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                    Text("データがありません😭", color = Color.Gray)
+                }
+            }
         }
     }
 }
