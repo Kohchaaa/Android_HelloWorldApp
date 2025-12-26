86a13b7cd98f88faf535c0021f563e0cf5f581b6
Kohchaaa <koustar2004@gmail.com>
2025-12-26 21:34:22 +0900

    chore: MealRecord型のestimationDetailをListに

--- app/src/main/java/com/example/helloworldapp/type/MealRecord.kt
+++ app/src/main/java/com/example/helloworldapp/type/MealRecord.kt
@@ -12,7 +12,7 @@ data class MealRecord(
     val imagePath: String?, // InputMethod == IMAGEの時に入る
     val mealName: String,
     val nutrients: Nutrients,
-    val estimationDetail: Ingredient // 推定された材料を構造化して記録
+    val estimationDetail: List<Ingredient> // 推定された材料を構造化して記録
 )
 
 enum class MealType { BREAKFAST, LUNCH, DINNER, SNACK, MIDNIGHT }
